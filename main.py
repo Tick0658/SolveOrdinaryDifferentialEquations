be0cf2f6381afeb6ced1b402f956fd0942f3ccda
keisuke <ronkun0658@yahoo.co.jp>
2024-05-08 22:46:05 +0900

    20240508

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 #plt.interactive(False)
 g = 9.80665 # 重力加速度(m/s^2)
-k = 0.0  # 空気抵抗係数(kg/m)
+k = 0.1  # 空気抵抗係数(kg/m)
 m = 10.0  # ボール重さ(kg)
 
-
-    
 def f(v):  # 関数の定義
-    #return (m * g - k * v**2) / m
-    return g
+    return (m * g - k * v**2) / m
+    #return g
 
 def rungekutta(dt,v):
     k1 = dt * f(v)
@@ -28,7 +25,7 @@ tarray = [0.0]  # 時間発展配列
 v = varray[0]  # 初期速度
 x = xarray[0]  # 初期位置
 t = tarray[0]  # 初期時間
-dt = 0.1e0  # ルンゲクッタ法の刻み幅
+dt = 0.01  # ルンゲクッタ法の刻み幅
 
 # ルンゲクッタ法のメイン計算
 while x >= 0.0e0:
